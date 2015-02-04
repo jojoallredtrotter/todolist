ba2f57e78ff118bfd7266ae223c75c9414c27283
Joverne Trotter <jojotrotter@gmail.com>
2015-02-04 10:29:39 -0500

    first commit

--- todolist/app/controllers/welcome_controller.rb
+++ todolist/app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
   def index
+  	@items = Item.all.order("dueby")
+  	@myuniquedates = []
+  	@items.each do |item|
+  		@myuniquedates = @myuniquedates.push(item.dueby)
   end
 end
+end
+
